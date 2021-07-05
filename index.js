7c38e81feee1625ccc9ce3fdbd65162315d4227e
Vadim Barabanov <introvert.png@gmail.com>
2021-07-05 21:25:23 +0300

    Base nodejs server

---
+++ index.js
@@ -0,0 +1,54 @@
+const http = require("http");
+const querystring = require("querystring");
+const fs = require("fs");
+
+const port = process.env.PORT || 4000;
+
+function respondText(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("hi");
+}
+
+function respondJson(req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ email: "vadim@vadim.com", name: "Vadim" }));
+}
+
+function repondNotFound(req, res) {
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.end("Not Found");
+}
+
+function respondEcho(req, res) {
+  const { input = "" } = querystring.parse(
+    req.url.split("?").slice(1).join("")
+  );
+
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      normal: input,
+      shouty: input.toUpperCase(),
+      characterCount: input.length,
+      backwards: input.split("").reverse().join(""),
+    })
+  );
+}
+
+function respondStatic(req, res) {
+  const filename = `${__dirname}/public${req.url.split("/static")[1]}`;
+  fs.createReadStream(filename)
+    .on("error", () => respondNotFound(req, res))
+    .pipe(res);
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") return respondText(req, res);
+  if (req.url === "/json") return respondJson(req, res);
+  if (req.url.match(/^\/echo/)) return respondEcho(req, res);
+  if (req.url.match(/^\/static/)) return respondStatic(req, res);
+
+  repondNotFound(req, res);
+});
+
+server.listen(port);
