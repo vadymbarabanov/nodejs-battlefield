3337052b1b1c79765218efbaf46016b489d73f7f
Vadim Barabanov <introvert.png@gmail.com>
2021-07-06 12:28:54 +0300

    Add chat log and previous messages features

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const fs = require("fs");
 const EventEmitter = require("events");
 const express = require("express");
 const chatEmitter = new EventEmitter();
+
 chatEmitter.on("message", console.log);
 
 const port = process.env.PORT || 4000;
 
 const app = express();
 
-app.get("/chat", respondChat);
-app.get("/sse", respondSSE);
-
 app.get("/", respondText);
 app.get("/json", respondJson);
 app.get("/echo", respondEcho);
 app.get("/static/*", respondStatic);
+
+app.get("/chat", respondChat);
+app.get("/sse", respondSSE);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
@@ -24,7 +25,25 @@ function respondSSE(req, res) {
     Connection: "keep-alive",
   });
 
+  fs.readFile(
+    `${__dirname}/log/chatLog.txt`,
+    { encoding: "utf8" },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      let buff = data.split("\n");
+
+      // omit last item in array because it's empty string
+      for (let i = 0; i < buff.length; i++) {
+        res.write(`data: ${buff[i]}\n\n`);
+      }
+    }
+  );
+
   const onMessage = (msg) => res.write(`data: ${msg}\n\n`);
+
   chatEmitter.on("message", onMessage);
   res.on("close", () => {
     chatEmitter.off("message", onMessage);
@@ -34,7 +53,14 @@ function respondSSE(req, res) {
 function respondChat(req, res) {
   const { message } = req.query;
 
-  chatEmitter.emit("message", message);
+  if (message) {
+    fs.appendFile(`${__dirname}/log/chatLog.txt`, message + "\n", (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    chatEmitter.emit("message", message);
+  }
   res.end();
 }
 
